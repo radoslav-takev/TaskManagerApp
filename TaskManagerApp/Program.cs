55009903301cd30c78e20e942cb2fa9b41f76b7d
Radoslav Takev <takev.radoslav@gmail.com>
2025-08-08 15:48:15 +0300

    Add project files.

---
+++ TaskManagerApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using TaskManagerApp;
+
+List<TaskItem> tasks = new List<TaskItem>();
+
+bool exit = false;
+
+
+while (!exit)
+{
+    Console.WriteLine("\n=== Task Manager ===");
+    Console.WriteLine("1. Add task");
+    Console.WriteLine("2. List tasks");
+    Console.WriteLine("3. Exit");
+    Console.WriteLine("Choose an option: ");
+
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            AddTask();
+            break;
+        case "2":
+            ListTasks();
+            break;
+        case "3":
+            exit = true;
+            Console.WriteLine("Bye Bye");
+            break;
+        default:
+            Console.WriteLine("Invalid choice.Try again from 1 to 3");
+            break;
+    }
+    void AddTask()
+    {
+        Console.Write("Enter task title: ");
+        string title = Console.ReadLine();
+
+        Console.Write("End due date (yyyy-mm-dd): ");
+        DateTime dueDate = DateTime.Parse(Console.ReadLine());
+
+        Console.Write("Enter Priority(Low,Medium,High): ");
+        Priority priority = Enum.Parse<Priority>(Console.ReadLine(),true);
+
+        TaskItem task = new TaskItem()
+        {
+            Title = title,
+            DueDate = dueDate,
+            Priority = priority,
+            IsCompleted = false
+        };
+
+        tasks.Add(task);
+        Console.WriteLine("Task Added!");
+    }
+    void ListTasks()
+    {
+        if (tasks.Count == 0)
+        {
+            Console.WriteLine("No tasks yet.");
+            return;
+        }
+        Console.WriteLine("\n--- Your Tasks ---");
+        for (int i = 0; i < tasks.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}.{tasks[i]}");
+        }
+    }
+}
---
+++ TaskManagerApp/TaskManagerApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TaskManagerApp
+{
+    public enum Priority
+    {
+        Low,
+        Medium,
+        High
+    }
+    public class TaskItem
+    {
+        public Priority Priority {  get; set; }
+        public string Title { get; set; }
+        public DateTime DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Title}";
+        }
+
+    }
+}
